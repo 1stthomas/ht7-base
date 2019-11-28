5b82da5a50d8e29d8430ebb8dc68f89fcbf3788b
1stthomas <thomas.pluess@mesch.ch>
2019-11-28 13:13:51 +0100

    make the name consistent to its kind

---
+++ src/Utility/Traits/CanLoadBySetMethod.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Ht7\Base\Utility\Traits;
+
+/**
+ *
+ * @author Thomas Plüss
+ */
+trait CanLoadBySetMethod
+{
+
+    public function load(array $data)
+    {
+        foreach ($data as $name => $value) {
+            $methodName = 'set' . ucfirst($name);
+
+            if (method_exists($this, $methodName)) {
+                $this->$methodName($value);
+            } else {
+                $this->$name = $value;
+            }
+        }
+    }
+
+}
