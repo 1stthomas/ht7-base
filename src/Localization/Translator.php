6f75cd64f09bf34d3199affbd8a4befa27a55a94
1stthomas <thomas.pluess@mesch.ch>
2020-01-28 22:05:03 +0100

    #14 make sure adapters can bew added and they are used

--- src/Localization/Translator.php
+++ src/Localization/Translator.php
@@ -2,84 +2,91 @@
 
 namespace Ht7\Base\Localization;
 
-use \InvalidArgumentException;
-
 /**
- * This class tries to find a registered translator. If there can not be found
- * one, the function <code>sprintf()</code> will be used to combine the several
- * text parts.
+ * This class tries to find a registered translation adapter. If there can not
+ * be found one, the function <code>sprintf()</code> will be used to combine the
+ * several text parts.
  *
  * @author Thomas Plüss
  */
 class Translator
 {
 
-    protected static $translator;
+    /**
+     * The object which defines the mapping for the translation methods.
+     *
+     * @var Translatorable
+     */
+    protected static $adapter;
 
     /**
-     * Get the current translator.
+     * Get the current translation adapter.
      *
      * In case this property was not set, an instance of this class will be returned.
      *
-     * @return type
+     * @return Translatorable
      */
-    public static function getTranslator()
+    public static function getAdapter()
     {
-        if (empty(static::$translator)) {
-            static::$translator = new static();
+        if (empty(static::$adapter)) {
+            // @todo: go through the registry!!
+            static::$adapter = new Adapter();
         }
 
-        return static::$translator;
+        return static::$adapter;
     }
 
     /**
-     * Set the current translator.
+     * Set the current translation adapter.
      *
-     * @param type $translator
+     * @param Translatorable $adapter
      */
-    public static function setTranslator($translator)
+    public static function setAdapter($adapter)
     {
-        static::$translator = $translator;
+        static::$adapter = $adapter;
     }
 
     /**
      * Translate a given text pattern with placeholders into a full text.
+     * This method supports only singular forms.
      *
-     * @param   integer     $type       The translation type. Use one of the
-     *                                  defined constants from this class.
-     * @param   mixed       $str        For simple and context translation types
+     * @param   string  $str            For simple and context translation types
      *                                  there is only one text pattern needed.
-     *                                  To make a plural translation working, in
-     *                                  minimum 2 text patterns are needed.
-     * @param   array $parameters
-     * @param   type $context           The context of the translation. This is
-     *                                  only needed for translation types
-     *                                  <code>Translator::TRANSLATION_TYPE_CONTEXT</code>.
-     * @return  string                  The translation.
+     * @param   array   $parameters
+     * @param   string  $context        The context of the translation. This is
+     *                                  only needed for contextual translations.
+     * @return  string                  The translated string.
      */
-    public static function t($type, $str, array $parameters, $context = '')
+    public static function t($str, array $parameters = [], $context = '')
     {
-        $return = null;
+        $adapter = static::getAdapter();
 
-        if (static::$translator === null) {
-            // Until we can set this proper into the registry and get the current
-            // translator interface, we do it this dirty way.
-            switch ($type) {
-                case TranslationTypes::TRANSLATION_TYPE_SIMPLE:
-                case TranslationTypes::TRANSLATION_TYPE_CONTEXT:
-                case TranslationTypes::TRANSLATION_TYPE_PLURAL:
-                    array_unshift($parameters, $str);
+        if (empty($context)) {
+            $return = $adapter->t1($str, $parameters);
+        } else {
+            $return = $adapter->t3($context, $str, $parameters);
+        }
 
-                    $return = call_user_func_array('sprintf', $parameters);
-                    break;
-                default:
-                    $msg = 'Undefined translation type %s';
-                    $e = static::t(TranslationTypes::TRANSLATION_TYPE_CONTEXT, $msg, [$type], 'ht7-base-testing');
+        return $return;
+    }
 
-                    throw new InvalidArgumentException($e);
-            }
-//        } else {
-            // @todo...
+    /**
+     *
+     * @param   string      $str1       Singular string to translate.
+     * @param   string      $str2       Plural string to translate.
+     * @param   array       $parameters
+     * @param   string      $context    The context of the translation. This is
+     *                                  only needed for contextual translations.
+     * @return  string                  The translated string.
+     */
+    public static function t2($str1, $str2, $count, array $parameters = [], $context = '')
+    {
+        $adapter = static::getAdapter();
+
+        if (empty($context)) {
+            $return = $adapter->t2($str1, $str2, $count, $parameters);
+        } else {
+            $return = $adapter->t4($context, $str1, $str2, $count, $parameters);
         }
 
         return $return;
