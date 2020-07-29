f20e40692bddebfa4e8cb92be84da7ffef4b13e7
1stthomas <thomas.pluess@mesch.ch>
2020-07-29 21:14:13 +0200

    #20 basic list interface implemented

--- src/Lists/AbstractItemList.php
+++ src/Lists/AbstractItemList.php
@@ -3,8 +3,6 @@
 namespace Ht7\Base\Lists;
 
 use \ArrayIterator;
-use \Countable;
-use \IteratorAggregate;
 use \Ht7\Base\Utility\Traits\CanRestrictInexVariables;
 
 /**
@@ -13,7 +11,7 @@ use \Ht7\Base\Utility\Traits\CanRestrictInexVariables;
  *
  * @author Thomas Pluess
  */
-abstract class AbstractItemList implements Countable, IteratorAggregate
+abstract class AbstractItemList implements ItemListable
 {
 
     use CanRestrictInexVariables;
@@ -26,28 +24,22 @@ abstract class AbstractItemList implements Countable, IteratorAggregate
     }
 
     /**
-     * Add an item to the end of the list.
-     *
-     * @param   mixed   $item           The item to add.
-     * @return  AbstractItemList         The current instance.
+     * {@inheritdoc}
      */
     public abstract function add($item);
 
     /**
      * Count elements of an object
      *
-     * @return  integer                 The number of items in the present item list.
+     * @return  integer                 The number of items of the present item list.
      */
     public function count()
     {
-        return count($this->items);
+        return count($this->getAll());
     }
 
     /**
-     * Get the item with the present index.
-     *
-     * @param   mixed       $index
-     * @return  mixed
+     * {@inheritdoc}
      */
     public function get($index)
     {
@@ -55,9 +47,7 @@ abstract class AbstractItemList implements Countable, IteratorAggregate
     }
 
     /**
-     * Get all items of this list.
-     *
-     * @return  array
+     * {@inheritdoc}
      */
     public function getAll()
     {
@@ -70,17 +60,15 @@ abstract class AbstractItemList implements Countable, IteratorAggregate
      * This method is the implementation of the IteratorAggergate interface,
      * which makes this class accessable through the foreach loop.
      *
-     * @return  ArrayIterator
+     * @return  \ArrayIterator
      */
     public function getIterator()
     {
-        return new ArrayIterator($this->items);
+        return new ArrayIterator($this->getAll());
     }
 
     /**
-     * Check if there is an item with the present index.
-     *
-     * @param   mixed   $index          The index to search the element with.
+     * {@inheritdoc}
      */
     public function has($index)
     {
@@ -88,10 +76,7 @@ abstract class AbstractItemList implements Countable, IteratorAggregate
     }
 
     /**
-     * Check if the item which matches the compare value is present in the
-     * current item list.
-     *
-     * @param   mixed   $compare         The compare value.
+     * {@inheritdoc}
      */
     public function hasByValue($compare)
     {
@@ -99,9 +84,7 @@ abstract class AbstractItemList implements Countable, IteratorAggregate
     }
 
     /**
-     * Load the items by the add method.
-     *
-     * @param   array       $data       The items to load into the present list.
+     * {@inheritdoc}
      */
     public function load(array $data)
     {
---
+++ src/Lists/ItemListable.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Ht7\Base\Lists;
+
+use \Countable;
+use \IteratorAggregate;
+
+/**
+ * Simple List Interface with basic manipulation methods.
+ *
+ * @author Thomas Pluess
+ */
+interface ItemListable extends Countable, IteratorAggregate
+{
+
+    /**
+     * Add an item to the end of the list.
+     *
+     * @param   mixed   $item           The item to add.
+     * @return  AbstractItemList         The current instance.
+     */
+    public function add($item);
+
+    /**
+     * Get the item with the present index.
+     *
+     * @param   mixed       $index
+     * @return  mixed
+     */
+    public function get($index);
+
+    /**
+     * Get all items of this list.
+     *
+     * @return  array
+     */
+    public function getAll();
+
+    /**
+     * Check if there is an item with the present index.
+     *
+     * @param   mixed   $index          The index to search the element with.
+     */
+    public function has($index);
+
+    /**
+     * Check if the item which matches the compare value is present in the
+     * current item list.
+     *
+     * @param   mixed   $compare         The compare value.
+     */
+    public function hasByValue($compare);
+
+    /**
+     * Load the items by the add method.
+     *
+     * @param   array       $data       The items to load into the present list.
+     */
+    public function load(array $data);
+
+    /**
+     * Merge two implementations of the ItemListable interface.
+     *
+     * @param \Ht7\Base\Lists\ItemListable  $iL     The ItemListable instance to
+     *                                              merge with the present one.
+     */
+    public function merge(ItemListable $iL);
+}
